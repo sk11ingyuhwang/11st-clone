61aa323596f8d8bc7e14163711cbffdc49190cee
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-27 17:52:51 +0900

    feat: App.vue에 Billboard vue 파일 경로 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <LNB />
   <TheHeader />
+  <Billboard />
   <!-- <RouterView /> -->
 </template>
 
 <script>
 import LNB from '~/components/LNB'
 import TheHeader from '~/components/TheHeader'
+import Billboard from '~/components/Billboard'
 
 export default {
   components: {
     LNB, //지역등록
-    TheHeader
+    TheHeader,
+    Billboard,
   }
 }
 </script>
